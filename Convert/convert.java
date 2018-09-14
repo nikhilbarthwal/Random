b751f7a72375e2a1c70c8431ca22d0c3664c01aa
Nikhil Barthwal <nikhilbarthwal@users.noreply.github.com>
2018-09-14 10:42:42 -0400

    update convert

--- Convert/convert.java
+++ Convert/convert.java
@@ -1,40 +1,65 @@
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class convert
 {
-    private static boolean tab = false;
+    //private static boolean tab = true;
 
     public static void main(String[] args) throws IOException
     {
         if (args.length <1) { System.out.println("No files to process!"); }
         else { for(String f : args) process(f); }
     }
-    
+
     private static void process(String f) throws IOException
     {
         System.out.println("Process File: " + f);
-        String s = new String(Files.readAllBytes(Paths.get(f)));
-        
+
         int l = 1;
         int p = 1;
+        byte sp = 32;
+        byte nl = 10;
+        byte tb = 9;
+
         ArrayList<Byte> data = new ArrayList<Byte>();
-        for(int i = 0; i< s.length(); i++)
+
+        for(String s : Files.readAllLines(Paths.get(f), Charset.defaultCharset()))
         {
-               int k = (int) s.charAt(i);
-               if (k==9) /* tab*/ { p++;if (tab) data.add((byte)9);
-                   else {byte b = 32; data.add(b);data.add(b);data.add(b);data.add(b);} }
-               else if (k==10) /* new Line */ { data.add((byte) k); l++; p=1;  } 
-               else if (k==13) /* carriage return */  {  p++; continue;  }
-               else if ((k>31) && (k<128)) { p++; data.add((byte) k); }
-               else { System.out.println("Illegal char at line: "+l+" position: "+p+" with value "+k); }
+            Stack<Byte> S = new Stack<Byte>();
+
+            for(int i = 0; i<s.length(); i++)
+            {
+                int k = (int) s.charAt(i);
+                    if (k==9)
+                    {
+                        if (f.equals("Makefile")||f.endsWith(".sln")) { p+=1; S.push(tb); }
+                        else { p+=4; S.push(sp); S.push(sp); S.push(sp); S.push(sp); }
+                    }
+                else if (k==13) /* carriage return */  p++;
+                else if ((k>31) && (k<128)) { p++; S.push((byte) k); }
+                else if (k==nl) { System.out.println("Illegal new line at line: " + l + " position: " + p); System.exit(1); }
+                else { System.out.println("Illegal char at line: " + l + " position: " + p + " with value " + k); p++; }
+            }
+
+            while ( (!S.empty()) && (S.peek() == sp) ) S.pop();
+            Stack<Byte> SS = new Stack<Byte>();
+            while (!S.empty()) SS.push(S.pop());
+            while (!SS.empty()) data.add(SS.pop());
+
+            data.add(nl); l++; p=1;
         }
 
-        byte[] byteArray = new byte[data.size()];
-        for(int i=0; i<data.size(); i++) byteArray[i]= data.get(i);
+        int n = data.size();
+        while(data.get(n-1) == nl) n--;
+
+        byte[] byteArray = new byte[1 + n];
+        for(int i=0; i<n; i++) byteArray[i]= data.get(i); byteArray[n] = nl;
         FileOutputStream fos = new FileOutputStream(f); fos.write(byteArray); fos.close();
-        System.out.println(data.size()+" bytes written to file "+f+"\n\n");
+        System.out.println(data.size() + " bytes written to file " + f + "\n\n");
     }
 }
