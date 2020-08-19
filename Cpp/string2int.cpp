6c8e8ba35295d5cae402d2e85f0b146965780c2b
Nikhil Barthwal <nikhilbarthwal@yahoo.com>
2020-08-18 19:43:20 -0700

    fix conversion

--- Cpp/string2int.cpp
+++ Cpp/string2int.cpp
@@ -4,6 +4,7 @@
 #include <stdexcept>
 #include <climits>
 #include <cctype>
+#include <cstdlib>
 
 using namespace std;
 
@@ -27,7 +28,7 @@ int Convert(const string &str) {
     const int n = s.length();
 
     if (n == 0)
-        throw runtime_error("Empty string!");
+        throw runtime_error("Empty string");
 
     bool sign = true;       // Assume the number is positive;
     int p = 0;              // Starting position for the string is beginning
@@ -43,27 +44,27 @@ int Convert(const string &str) {
 
     for (int i = p; i < n; i++)
         if ((s[i] < '0') || (s[i] > '9'))
-            throw runtime_error("Invalid format!");
+            throw runtime_error("Invalid format");
 
     while (s[p] == '0') // Optimization: Trim all leading zeroes
         p++;
 
     const int limit = sign ? INT_MAX : INT_MIN;
     int result = 0;
-    int exp = 1;
+    int exp = (sign) ? 1 : -1;
     for (int i = n - 1; i >= p; i--) {
         int delta = exp * (s[i] - '0');
 
         // Check if there is more room to add, then add else throw overflow exception
-        if ((sign) ? ((limit - result) >= delta) : ((limit - result) <= -1 * delta))
-            result += ((sign) ? 1 : -1) * delta;
+        if ((sign) ? ((limit - result) >= delta) : ((limit - result) <= delta))
+            result += delta;
         else
             throw overflow_error("Integer overflow");
 
 
         // If the limit of # of digits have reached and there are still more digits left
         // to be processed, then throw overflow exception.
-        if ((INT_MAX / exp < (double) 10) && (i > p))
+        if ((((double) limit / exp) < (double) 10) && (i > p))
             throw overflow_error("Integer overflow");
         else
             exp *= 10;
@@ -72,10 +73,26 @@ int Convert(const string &str) {
     return result;
 }
 
+void test(string str)
+{
+    cout<<"\""<<str<<"\"\t-->\t";
+    try
+    {
+        cout<<Convert(str)<<endl;
+    }
+    catch(const exception& e)
+    {
+        cout<<"EXCEPTION - "<<e.what()<<endl;
+    }
+
+}
+
+
 int main(void) {
-    //cout<<Convert("+00000000000000002147483647")<<endl;
-    cout << Convert("-00000000000000002147483648") << endl;
-    cout << Convert("00000000000000002147483647") << endl;
+    test("+000002147483647");
+    test("-0000002147483648");
+    test("    02147483648  ");
+    test(" adfasfdgher ewr");
     return 0;
 }
 
